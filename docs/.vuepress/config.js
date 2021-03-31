1ebcff68562ee4bb9a647067e5c9dbfc3e7fa098
Wsq <wangsq0926@163.com>
2021-03-31 17:46:53 +0800

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: '开发笔记',
   description: '对开发和学习过程中遇到的问题进行记录总结',
-  base: process.env.NODE_ENV  === '' ? 'devNote' : '/',
+  base: process.env.NODE_ENV === '' ? 'devNote' : '/',
   // 侧边栏
   themeConfig: {
     // displayAllHeaders: true,
