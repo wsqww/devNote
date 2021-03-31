156bcb5bd3993759e7dd6349422b8f2f63facf17
Wsq <wangsq0926@163.com>
2021-03-31 17:14:46 +0800

    add: 添加 vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: '开发笔记',
+  description: '对开发和学习过程中遇到的问题进行记录总结',
+  // 侧边栏
+  themeConfig: {
+    // displayAllHeaders: true,
+    sidebar: [
+      {title: '介绍', path: '/'},
+      {title: 'Angular', path: '/Angular/'},
+      {title: 'Vue', path: '/Vue/'},
+      {title: 'NodeJs', path: '/NodeJs/'},
+    ]
+  }
+}
