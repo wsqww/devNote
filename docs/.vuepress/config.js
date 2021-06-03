83ef48a40c299bc2a8bbf92abaa63c283021208c
Wsq <wangsq0926@163.com>
2021-06-03 15:59:57 +0800

    fix: 修改 ico 图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   title: '开发笔记',
   description: '对开发和学习过程中遇到的问题进行记录总结',
   base: process.env.NODE_ENV === 'production' ? '/devNote/' : '/',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   // 侧边栏
   themeConfig: {
     // displayAllHeaders: true,
